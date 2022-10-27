a99131f09cc30080ac8b3ba4527aa182f69def59
Tomas Izuel <tomasizuel@gmail.com>
2022-10-27 13:26:14 -0300

    cambiando el JS a orientado a objetos

--- js.js
+++ js.js
@@ -1,37 +1,53 @@
-const tabla = document.getElementById("tabla")
+const matrizhtml = document.getElementById("matriz")
 
+let funcion = new Array
+let constantes = new Array
+let entrada = new Array
+let iniciales = new Array
 
-function enviar() {
-    const deltat = document.getElementById("deltat").value
-    const w = document.getElementById("w").value
-    const x3 = document.getElementById("x3").value
-    let x2 = (document.getElementById("x2").value)*(-1)
-    const x1 = (document.getElementById("x1").value)*(-1)
-    const x0 = (document.getElementById("x0").value)*(-1)
-    const result = document.getElementById("result").value
-    const resultf = document.getElementById("result-f").value
-    const plus = document.getElementById("plus").value
-    const ym1 = document.getElementById("ym1").value
-    const ym2 = document.getElementById("ym2").value
-    const ym3 = document.getElementById("ym3").value
-    if(x3 == 0){
-        x2 = x2 * (-1)
-        const X1 = x1/x2;
-        const X0 = x0/x2;
-        const RESULT = result/x2;
-        const PLUS = plus/x2;
-        let f;
-        let ym = [ym1,ym2]
-        if(x2 == 0){
-            f= [X1,X0,1,0]
-            tabla.innerHTML = `<div class="tablas"><table> <tr> <td>x2'</td> </tr> <tr> <td>x1'</td> </tr> </table> <span> = </span> <table> <tr> <td>${x1}</td> <td>${x0}</td> </tr> <tr> <td>1</td> <td>0</td> </tr> </table> <span> + </span> <table> <tr> <td>${result} ${resultf}</td> </tr> <tr> <td>0</td> </tr> </table> <span> + </span> <table> <tr> <td>${plus}</td> </tr> <tr> <td>0</td> </tr> </table></div>`;
-        }else{
-            f= [x1,x0,1,0]
-            tabla.innerHTML = `<div class="tablas"><table> <tr> <td>x2'</td> </tr> <tr> <td>x1'</td> </tr> </table> <span> = </span> <table> <tr> <td>${X1}</td> <td>${X0}</td> </tr> <tr> <td>1</td> <td>0</td> </tr> </table> <span> + </span> <table> <tr> <td>${RESULT} ${resultf}</td> </tr> <tr> <td>0</td> </tr> </table> <span> + </span> <table> <tr> <td>${PLUS}</td> </tr> <tr> <td>0</td> </tr> </table></div>`;
+class FuncionRungeKutta{
+    constructor(constantes, iniciales, funcion, entrada){
+        this.constantes = constantes;
+        this.iniciales = iniciales;
+        this.funcion = funcion;
+        this.entrada = entrada;
+        this.matriz = new Array;
+    }
+    obtenerMatriz(){
+        if(funcion[3] == 0){
+            let funcion2x2 = new Array
+            if(funcion[2] == 0){
+                funcion2x2[0] = funcion[1] * (-1)
+                funcion2x2[1] = funcion[0] * (-1)
+                funcion2x2[2] = 1
+                funcion2x2[3] = 0
+                matrizhtml.innerHTML = `<div class="tablas"><table> <tr> <td>x2'</td> </tr> <tr> <td>x1'</td> </tr> </table> <span> = </span> <table> <tr> <td>${funcion2x2[0]}</td> <td>${funcion2x2[1]}</td> </tr> <tr> <td>${funcion2x2[2]}</td> <td>${funcion2x2[3]}</td> </tr> </table> <table> <tr> <td> x2(t) </td> </tr> <tr> <td> x1(t) </td> </tr> </table> <span> + </span> <table> <tr> <td>${entrada[0]} ${entrada[1]}</td> </tr> <tr> <td>0</td> </tr> </table> <span> + </span> <table> <tr> <td>${entrada[2]}</td> </tr> <tr> <td>0</td> </tr> </table></div>`;
+            }else{
+                funcion2x2[0] = (funcion[1] * (-1)) / funcion[2]
+                funcion2x2[1] = (funcion[0] * (-1)) / funcion[2]
+                funcion2x2[2] = 1
+                funcion2x2[3] = 0
+                matrizhtml.innerHTML = `<div class="tablas"><table> <tr> <td>x2'</td> </tr> <tr> <td>x1'</td> </tr> </table> <span> = </span> <table> <tr> <td>${funcion2x2[0]}</td> <td>${funcion2x2[1]}</td> </tr> <tr> <td>${funcion2x2[2]}</td> <td>${funcion2x2[3]}</td> </tr> </table> <table> <tr> <td> x2(t) </td> </tr> <tr> <td> x1(t) </td> </tr> </table> <span> + </span> <table> <tr> <td>${entrada[0]} ${entrada[1]}</td> </tr> <tr> <td>0</td> </tr> </table> <span> + </span> <table> <tr> <td>${entrada[2]}</td> </tr> <tr> <td>0</td> </tr> </table></div>`;
+            }
+            this.matriz = funcion2x2
         }
-        tabla2x2(f,deltat,w,ym);
     }
 }
 
-const tabla2x2 = (f,delta, w, ym) => {
+
+function enviar() {
+    constantes[0] = document.getElementById("deltat").value
+    constantes[1] = document.getElementById("w").value
+    iniciales[0] = document.getElementById("ym0").value
+    iniciales[1] = document.getElementById("ym1").value
+    iniciales[2] = document.getElementById("ym2").value
+    funcion[3] = document.getElementById("x3").value
+    funcion[2] = document.getElementById("x2").value
+    funcion[1] = document.getElementById("x1").value
+    funcion[0] = document.getElementById("x0").value
+    entrada[0] = document.getElementById("result").value
+    entrada[1] = document.getElementById("result-f").value
+    entrada[2] = document.getElementById("plus").value
+    const funcionRungeKutta = new FuncionRungeKutta(constantes, iniciales, funcion, entrada)
+    funcionRungeKutta.obtenerMatriz();
 }
